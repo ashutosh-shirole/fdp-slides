a3ca4a6ab30466f999d27157bcd75736d7b3331a
root <root@localhost.localdomain>
2016-11-29 05:11:27 +0100

    scripts

---
+++ scripts/infernal-setup.sh
@@ -0,0 +1,11 @@
+#apt-get install apache2
+
+apt-get install mysql-server libapache2-mod-auth-mysql php5-mysql
+
+apt-get install python-scapy
+
+apt-get install python-wxtools
+
+apt-get install python-mysqldb
+
+#git clone https://github.com/entropy1337/infernal-twin.git
